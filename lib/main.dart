016bebfb5f51cfe6eb66ea74843a93cf28887910
uzvn <uzanmuyumbaben@gmail.com>
2025-12-26 23:05:20 +0200

    feat(main): rollDice setState methode

--- lib/main.dart
+++ lib/main.dart
@@ -21,23 +21,24 @@ class PigCeeLo extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DicePage> createState() => _DicePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DicePageState extends State<DicePage> {
+  int _currentDiceValue = 1;
+  final Random _random = Random();
 
-  void _incrementCounter() {
+  void _rollDice() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      _currentDiceValue =
+          _random.nextInt(6) + 1; // Random number between 1 and 6
+    });
+  }
+
       _counter++;
     });
   }
